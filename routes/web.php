ad2df66fad01cc56127d7c36b8819c452fd3844d
ihahachi <iha2007hachi@gmail.com>
2018-04-30 11:50:12 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/* Route::get('/', function () {
+    return view('welcome');
+}); */
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
+
+
+Route::get('/', function () {
+    $posts = App\Post::all();
+    return view('welcome', compact('posts'));
+});
